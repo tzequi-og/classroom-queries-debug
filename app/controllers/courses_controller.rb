e9bd0170d377ae5638bc6bfa3a0b98e59a28d2f0
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2023-11-06 15:26:45 +0000

    amost_done

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -27,7 +27,7 @@ class CoursesController < ApplicationController
   end
 
   def update
-    id = params.fetch("path_id")
+    the_id = params.fetch("path_id")
     @course = Course.where({ :id => the_id }).at(0)
 
     @course.title = params.fetch("query_title")
